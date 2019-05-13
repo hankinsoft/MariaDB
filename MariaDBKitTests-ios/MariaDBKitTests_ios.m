0328b3c4a321b6e33344d2cc38cd4911181ef395
Kyle Hankinson <kyle@hankinsoft.com>
2019-05-13 15:20:51 -0300

    + Added support for tls.

--- MariaDBKitTests-ios/MariaDBKitTests_ios.m
+++ MariaDBKitTests-ios/MariaDBKitTests_ios.m
@@ -25,7 +25,7 @@
     [super tearDown];
 }
 
-- (void)testExample {
+- (void) testConnect {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
 }
